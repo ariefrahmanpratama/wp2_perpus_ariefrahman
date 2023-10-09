e129747ae89db598979c0a02e88c8752cc905c3d
ariefrahmanpratama <rahmanarief111@gmail.com>
2023-10-09 16:16:25 +0700

    sudah menambahkan pertemuan 4

--- application/views/view-data-matakuliah.php
+++ application/views/view-data-matakuliah.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tampil data matakuliah</title>
+    <title>Tampil data matakuliah</title>
 </head>
 <body>
     <center>
